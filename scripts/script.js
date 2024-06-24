00ce560ab4ab61aa2fb63912983c42a00f57d463
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-06-24 19:52:07 -0300

    menu mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 import initDropdown from './dropdown.js';
-import initDropdown from './menuMobile.js';
+import initMenuMobile from './menuMobile.js';
 
 
 
@@ -7,8 +7,58 @@ import initDropdown from './menuMobile.js';
 
 
 initDropdown();
+initMenuMobile();
+
+
+// Menu mobile
+
+const menuButton = document.querySelector('#btn');
+const menuList = document.querySelector('#menu');
+
+
+function openMenu(event){
+
+    menuList.classList.toggle('active');
+    menuButton.classList.add('active');
+
+
+}
+menuButton.addEventListener('click', openMenu);
 
 
 
 
 
+
+
+
+
+
+// dropdown
+
+const dropdown = document.querySelectorAll('[data-dropdown]');
+
+dropdown.forEach(menu =>{
+    ['touchstart', 'click'].forEach(userEvent =>{
+        menu.addEventListener(userEvent, handleClick);
+    })
+});
+
+function handleClick(event){
+    event.preventDefault();
+    this.classList.add('Active');
+    outsideClick(this, () =>{
+        this.classList.remove('active');
+    });
+};
+function outsideClick(callback){
+    const html = document.documentElement;
+    html.addEventListener('click', handleOutsideClick);
+    function handleOutsideClick(event){
+        if(!element.contains(event.target)){
+            html.removeEventListener('click', handleOutsideClick);
+            callback();
+        }
+       
+    }
+}
